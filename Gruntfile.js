ae4f1716dd18aa3d5bcf39bf0edd2f8b6e1c9b98
Thomas Boyt <me@thomasboyt.com>
2013-04-14 15:30:45 -0400

    accidental global

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,7 @@ module.exports = function(grunt) {
         
         var compiler = new Compiler(grunt.file.read(path), name, options);
 
+        var format;
         switch (options.format) {
           case 'amd':
             format = compiler.toAMD;
